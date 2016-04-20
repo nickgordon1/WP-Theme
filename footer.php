87d4bf0c7e3f99fc3507ade290df10a0a9a1cfc9
Nick Gordon <nicholasjgordon@hotmail.com>
2016-04-20 15:50:01 -0500

    Integrated jQuery and Bootstrap
    Using bootstrap to clean up the pages and make them responsive (mainly
    worked on nav and blog will be next)

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
-  <footer>
-    <p>Footer</p>
-     <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-  </footer>
+    <footer>
+      <p>Footer</p>
+       <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+    </footer>
+
+  </div><!--.container-->
 
   <?php wp_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -1,13 +1,28 @@
 <?php
 
+  /*
+  =========================
+  Include Scripts
+  =========================
+  */
+
 function wptheme_script_enqueue () {
-  
+  //CSS
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . 'css/bootstrap.min.css', array() , '3.3.6', 'all');
   wp_enqueue_style('customstyle', get_template_directory_uri() . 'css/style.css', array() , '1.0.0', 'all');
+  //JS
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrapjs', get_template_directory_uri() . 'js/bootstrap.min.js', array(), '3.3.6', true); 
   wp_enqueue_script('customjs', get_template_directory_uri() . 'js/wptheme.js', array(), '1.0.0', true);
-    
 }
 
 add_action('wp_enqueue_scripts', 'wptheme_script_enqueue');
+
+  /*
+  =========================
+  Activate Menus
+  =========================
+  */
 
 function wptheme_theme_setup(){
   
@@ -19,6 +34,12 @@ function wptheme_theme_setup(){
 }
 
 add_action('init', 'wptheme_theme_setup');
+
+  /*
+  =========================
+  Theme Support Fucntions
+  =========================
+  */
 
 add_theme_support('custom-background');
 add_theme_support('custom-header');
